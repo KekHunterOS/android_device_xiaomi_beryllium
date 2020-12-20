520bec9e1c21e9bb962ec16543fbe7d7f032cfb4
Akhil Narang <me@akhilnarang.dev>
2020-12-20 22:18:02 +0530

    beryllium: switch to coral January 2021 build fingerprint
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- aosip_beryllium.mk
+++ aosip_beryllium.mk
@@ -16,7 +16,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.2.0.QEJMIXM release-keys" \
