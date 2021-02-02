06520e176db78ba198d96a13307af60707b25f0c
Hilledkinged <kailing2@live.com>
2021-02-02 20:36:43 +0200

    Changes for kernel.mk

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -1,10 +1,19 @@
 ifneq ($(filter beryllium, $(TARGET_DEVICE)),)
 
-IMAGE_GZ := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/Image.gz
-DTB := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/sdm845-beryllium.dtb
+IMAGE_GZ := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/kernel/Image.gz
 
-$(PRODUCT_OUT)/kernel: $(IMAGE_GZ) $(DTB)
+DTB_EBBG := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/dtb/sdm845-xiaomi-beryllium-ebbg.dtb
+DTB_TIANMA := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/dtb/sdm845-xiaomi-beryllium-tianma.dtb
+DTB_MTP := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/dtb/sdm845-mtp.dtb
+
+
+# For EBBG
+$(PRODUCT_OUT)/kernel: $(IMAGE_GZ) $(DTB_EBBG) $(DTB_MTP)
 	cat $(IMAGE_GZ) $(DTB) > $@
+
+# Fot TIANMA
+#$(PRODUCT_OUT)/kernel: $(IMAGE_GZ) $(DTB_TIANMA)
+#	cat $(IMAGE_GZ) $(DTB) > $@
 
 droidcore: $(PRODUCT_OUT)/kernel
 
